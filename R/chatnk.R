bae4d2293aa2579a877e7851f4aa5cdb9ce7268f
MurrayEfford <MurrayEfford@users.noreply.github.com>
2023-04-26 22:00:55 +1200

    minor re-write

--- R/chatnk.R
+++ R/chatnk.R
@@ -2,6 +2,7 @@
 ## package 'secr'
 ## chatnk.R
 ## 2022-11-18, 29
+## 2023-04-26
 ###############################################################################
 
 Fletcher.chat <- function(observed, expected, np) {
@@ -26,33 +27,26 @@ Fletcher.chat <- function(observed, expected, np) {
 
 chat.nk.sess <- function(object, D, capthist, mask, detpar) {
     
-    if (ms(object)) {
-        stop("expect single session")
-    }
-    else {
-        ## check PIA0 for variation over occasions, animals, detectors?
-        ## substitute detectpar by lookup of Xrealparval0 with PIA0?
-        
-        expected.nk <- Enk(
-            D          = D, 
-            mask       = mask, 
-            traps      = traps(capthist), 
-            detectfn   = object$detectfn, 
-            detectpar  = detpar, 
-            noccasions = dim(capthist)[2],
-            binomN     = object$binomN, 
-            userdist   = object$details$userdist, 
-            ncores     = NULL)
-        
-        nk <- apply(apply(abs(capthist),c(1,3),sum)>0, 2, sum)
-        
-        np <- length(object$betanames)
-        if (np > (nrow(traps(capthist))-1)) stop ("c-hat not estimated when np > K-1")
-
-        Fletcher.chat(nk, expected.nk, np)
-        
-        ##-------------------------------------------------------------
-    }
+    ## check PIA0 for variation over occasions, animals, detectors?
+    ## substitute detectpar by lookup of Xrealparval0 with PIA0?
+    expected.nk <- Enk(
+        D          = D, 
+        mask       = mask, 
+        traps      = traps(capthist), 
+        detectfn   = object$detectfn, 
+        detectpar  = detpar, 
+        noccasions = dim(capthist)[2],
+        binomN     = object$binomN, 
+        userdist   = object$details$userdist, 
+        ncores     = NULL)
+    
+    nk <- apply(apply(abs(capthist),c(1,3),sum)>0, 2, sum)
+    
+    np <- length(object$betanames)
+    if (np > (nrow(traps(capthist))-1)) stop ("c-hat not estimated when np > K-1")
+    
+    Fletcher.chat(nk, expected.nk, np)
+    
 }
 
 chat.nk <- function(object) {
@@ -67,12 +61,14 @@ chat.nk <- function(object) {
         else {
             Dlist <- lapply(covariates(predictDsurface(object)), '[[', 'D.0')
         }
-        detparlist <- detectpar(object)
-        ch <- object$capthist
-        object$capthist <- object$capthist[[1]] # to fool not ms
+        getdet <- function(x) {
+            ok <- rownames(x) %in% c('g0','lambda0','sigma','z')
+            as.list(setNames(x[ok,'estimate'], rownames(x)[ok]))
+        }
+        detparlist <- lapply(predict(object), getdet)
         mapply(chat.nk.sess, 
             D = Dlist, 
-            capthist = ch, 
+            capthist = object$capthist,   # 2023-04-23 previously object$capthist[[1]] 
             mask = object$mask, 
             detpar = detparlist, 
             MoreArgs = list(object = object), 
@@ -166,24 +162,3 @@ adjustVarD <- function(object, chatmin = 1.0, alpha = 0.05, chat = NULL) {
         MoreArgs = list(chatmin = chatmin, alpha = alpha),
         SIMPLIFY = FALSE))
 }
-
-# #-------------------------------------------------------------------------------
-# # cf Paul Hiemstra Aug 23 2012 StackOverFlow
-# binarystring <- function(number, noBits) {
-#     binary_vector = rev(as.numeric(intToBits(number)))
-#     paste(binary_vector[-(1:(length(binary_vector) - noBits))], collapse='')
-# }
-# 
-# # steps towards overdispersion of detection Nov 2022
-# 
-# chat.i <- function (object) {
-#     ch <- object$capthist
-#     noccasions <- ncol(ch)
-#     chis <- apply(abs(ch),1:2,sum)
-#     chis <- pmin(chis,1) # ignore detection at multiple traps
-#     observedCH <- apply(chis,1,paste, collapse='')
-#     possibleCH <- sapply(1:2^noccasions, binarystring,noccasions)
-#     table(factor(observedCH, levels = possibleCH))  
-# }
-# # chat.i(secrdemo.0)
-# #-------------------------------------------------------------------------------
