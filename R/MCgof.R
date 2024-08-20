6423db83c243d72370924743f2b6397f4db11b73
MurrayEfford <murray.efford@otago.ac.nz>
2024-08-21 07:07:28 +1200

    development

--- R/MCgof.R
+++ R/MCgof.R
@@ -7,6 +7,19 @@
 # Murray Efford and Ran Yu Choo
 ################################################################################
 
+defaultstatfn <- function (capthist) {
+    list(
+        yik = apply(abs(capthist), c(1,3), sum),
+        yi = apply(abs(capthist), 1, sum),
+        yk = apply(abs(capthist), 3, sum)
+    )
+}
+
+# Calculate Freeman-Tukey statistic, from Choo et al. 2024
+defaulttestfn <- function(realised, expected){
+    sum((sqrt(realised) - sqrt(expected))^2)
+}
+
 MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
                    seed = NULL, ncores = 1, quiet = FALSE)
 
@@ -14,13 +27,14 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
     #---------------------------------------------------------------------------
     ## function to run one replicate
     runone <- function(i=1) {
-        
+        # -----------------------------------
         # randomize parameter values from MVN
         object$fit$par <- MASS::mvrnorm(1,
                                         mu = object$fit$par, 
                                         Sigma = object$beta.vcv)
         
-        # sample one location of each observed animal from its pdf
+        # -----------------------------------
+        # sample one location of each _observed_ animal from its pdf
         fxi.list <- fxi.secr(object)
         mask <- object$mask
         sp   <- spacing(mask) # cell size
@@ -30,37 +44,53 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
         attr(obspop, 'boundingbox') <- attr(mask, 'boundingbox')
         rownames(obspop) <- names(fxi.list)
         
+        # -----------------------------------
         # density of unobserved AC
         # this code is also in fx.total
         D <- predictDsurface(object)
         D <- covariates(D)$D.0
         N <- sum(D) * attr(mask, 'area')
-        if (N<nrow(CH)) warning("simulated N less than number observed")
-        detectpar <- detectpar(object)  # simple only... consider byclass = TRUE
-        pd <- pdot(X = mask, traps = traps(CH), detectfn = object$detectfn,
-                   detectpar = detectpar, noccasions = ncol(CH), ncores = ncores)
+
+        # make a null starting popn
+        unobspop <- sim.popn(0, data.frame(x=0,y=0), covariates = NULL) 
+
+        if (N>nrow(CH)) {
+            detpar <- detectpar(object, byclass = TRUE)  
+            if (is.na(detpar[[1]]['pmix'])) detpar <- list(c(detpar, pmix=1))
+            for (i in 1:length(detpar)) {
+                pd <- pdot(X = mask, traps = traps(CH), detectfn = object$detectfn,
+                           detectpar = detpar[[i]], noccasions = ncol(CH), ncores = ncores)
+                # sample locations of unobserved AC for i-th class
+                pop <- sim.popn (D = D * (1 - pd) * detpar[[i]]$pmix, core = mask, model2D = 'IHP', 
+                                 Ndist = 'fixed', covariates = NULL)
+                unobspop <- rbind(unobspop, pop)
+            }
+            rownames(unobspop) <- paste0('N', 1:nrow(unobspop))
+        }
+        else {
+            if (N<nrow(CH)) warning("proposed N less than number observed; unobserved not simulated")
+            else warning("proposed N equal to number observed; unobserved not simulated")
+        }
         
-        # sample locations of unobserved AC
-        unobspop <- sim.popn (D = D * (1 - pd), core = mask, model2D = 'IHP', 
-                              Ndist = 'fixed', covariates = NULL)
-        rownames(unobspop) <- paste0('N', rownames(unobspop))
-        
+        # -----------------------------------
         # combine observed, unobserved
         popn <- rbind(obspop, unobspop, renumber = FALSE)
         Nobs <- nrow(obspop)
         Nunobs <- nrow(unobspop)  # varies because D varies
         covariates(popn) <- data.frame(obs = rep(c(TRUE,FALSE), c(Nobs, Nunobs)))
         
+        # -----------------------------------
         ## add individual covariates that may be needed by sim.detect
         if (!is.null(object$hcov)) {
             ## for unobs, sample with replacement from original hcov field
             oldhcov <- covariates(CH)[,object$hcov]
             covariates(popn)[[object$hcov]] <- 
                 sample(oldhcov, size = nrow(popn), replace = TRUE)
-            ## then overwrite obs hcov
-            covariates(popn)[1:Nobs,object$hcov] <- covariates(obspop)[,object$hcov]
+            ## then overwrite obs (known) hcov
+            covariates(popn)[1:Nobs,object$hcov] <- covariates(object$capthist)[,object$hcov]
         }
         
+        # -----------------------------------
         # using sim.detect revised 2024-08-02
         simCH <- sim.detect(object, popnlist = list(popn), expected = TRUE, 
                             dropzeroCH = FALSE, renumber = FALSE)
@@ -74,25 +104,28 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
         list(Tsim = Tsim, Texp = Texp, par = object$fit$par, popn = popn)
         
     }   # end of runone
+
     #---------------------------------------------------------------------------
-    
+    # construct test statistic for simulation 'capti' and marginal counts 'stat'
+    ft <- function (capti, stat) {
+        N <- nrow(capti$popn)  # varies for each replicate because D varies
+        n <- nrow(CH)
+        # addzeroCH is internal secr function
+        paddedCH <- addzeroCH(CH, N - n, prefix = 'N')
+        Tobs <- statfn(paddedCH)
+        obs <- testfn(Tobs[[stat]],       capti$Texp[[stat]])
+        sim <- testfn(capti$Tsim[[stat]], capti$Texp[[stat]])
+        c(Tobs = obs, Tsim = sim, p = sim>obs)  
+    }   # end of ft
+    #---------------------------------------------------------------------------
     
     ## create default statfn if needed
     if (is.null(statfn)) {
-        statfn <- function (capthist) {
-            list(
-                yik = apply(abs(capthist), c(1,3), sum),
-                yi = apply(abs(capthist), 1, sum),
-                yk = apply(abs(capthist), 3, sum)
-            )
-        }
+        statfn <- defaultstatfn
     }
     ## create default testfn if needed
     if (is.null(testfn)) {
-        # Calculate Freeman-Tukey statistic, from Choo et al. 2024
-        testfn <- function(realised, expected){
-            sum((sqrt(realised) - sqrt(expected))^2)
-        }
+        testfn <- defaulttestfn
     }
     
     #---------------------------------------------------------------------------
@@ -142,9 +175,11 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
     
     CH <- object$capthist
     
-    ## optional parallel processing - often slower
-    
-    if (is.null(ncores)) ncores <- setNumThreads()
+    # ----------------------------------------------------------
+    # perform nsim simulations
+    ## parallel processing optional - often slower
+
+    if (is.null(ncores)) ncores <- setNumThreads()  # default is 1
     if (!quiet && ncores == 1) pb <- txtProgressBar(0, nsim, style = 3)
 
     if (ncores > 1) {
@@ -161,22 +196,16 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
     else {
         capt <- lapply(1:nsim, runone)
     }
-
-    ft <- function (capti, stat) {
-        N <- nrow(capti$popn)  # varies for each replicate because D varies
-        n <- nrow(CH)
-        # addzeroCH is internal secr function
-        paddedCH <- addzeroCH(CH, N - n, prefix = 'N')
-        Tobs <- statfn(paddedCH)
-        obs <- testfn(Tobs[[stat]],       capti$Texp[[stat]])
-        sim <- testfn(capti$Tsim[[stat]], capti$Texp[[stat]])
-        c(Tobs = obs, Tsim = sim, p = sim>obs)  
-    }
     
-    tests <- c('yik','yi','yk')
-    all <- lapply(tests, function(x) sapply (capt, ft, stat = x))
-    names(all) <- tests
-
+    # ----------------------------------------------------------
+    # apply function 'ft' for each count vector 'stat'
+    # stats correspond to named components of statfn output list
+    stats <- c('yik','yi','yk')
+    all <- lapply(stats, function(x) sapply (capt, ft, stat = x))
+    names(all) <- stats
+    
+    # ----------------------------------------------------------
+    # wrap up
     out <- list(
         statfn   = statfn,
         testfn   = testfn,
