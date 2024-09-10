cb26294bb5687806047a0e271c2dfc4f5f3a235b
MurrayEfford <murray.efford@otago.ac.nz>
2024-09-10 19:10:47 +1200

    export MCgof

--- R/MCgof.R
+++ R/MCgof.R
@@ -3,8 +3,12 @@
 ## MCgof.R
 
 ## 2024-07-29 started, based on simulate.secr
+## 2024-09-03 flexible naming of statfn output
+## 2024-09-03 new args usefxi, useMVN
+## 2024-09-03 call sim.onepopn from simulate.R if !usefxi
+## 2024-09-09 various edits
 
-# Murray Efford and Ran Yu Choo
+# Murray Efford and Yan Ru Choo
 ################################################################################
 
 defaultstatfn <- function (capthist) {
@@ -20,50 +24,61 @@ defaulttestfn <- function(realised, expected){
     sum((sqrt(realised) - sqrt(expected))^2)
 }
 
-MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
-                   seed = NULL, ncores = 1, quiet = FALSE)
+simfxiAC <- function (object, bytrap, dN = TRUE) {
 
-{
-    #---------------------------------------------------------------------------
-    ## function to run one replicate
-    runone <- function(i=1) {
-        # -----------------------------------
-        # randomize parameter values from MVN
-        object$fit$par <- MASS::mvrnorm(1,
-                                        mu = object$fit$par, 
-                                        Sigma = object$beta.vcv)
-        
-        # -----------------------------------
-        # sample one location of each _observed_ animal from its pdf
-        fxi.list <- fxi.secr(object)
-        mask <- object$mask
-        sp   <- spacing(mask) # cell size
-        m <- sapply(fxi.list, sample.int, n = nrow(mask), size = 1, replace=TRUE)
-        obspop <- mask[m,] + runif(length(m)*2, -sp/2, +sp/2)  # jitter within cells
-        class (obspop) <- c('popn', 'data.frame')
-        attr(obspop, 'boundingbox') <- attr(mask, 'boundingbox')
-        rownames(obspop) <- names(fxi.list)
-        
-        # -----------------------------------
-        # density of unobserved AC
-        # this code is also in fx.total
-        D <- predictDsurface(object)
-        D <- covariates(D)$D.0
+    # -----------------------------------
+    # sample one location of each _observed_ animal from its pdf
+    fxi.list <- fxi.secr(object)
+    mask <- object$mask
+    sp   <- spacing(mask) # cell size
+    m <- sapply(fxi.list, sample.int, n = nrow(mask), size = 1, replace=TRUE)
+    obspop <- mask[m,] + runif(length(m)*2, -sp/2, +sp/2)  # jitter within cells
+    class (obspop) <- c('popn', 'data.frame')
+    attr(obspop, 'boundingbox') <- attr(mask, 'boundingbox')
+    rownames(obspop) <- names(fxi.list)
+    
+    # -----------------------------------
+    # sample unobserved AC from their pdf
+    
+    # density of unobserved AC
+    # this code is also in fx.total
+    D <- predictDsurface(object)
+    D <- covariates(D)$D.0
+    
+    # make a null starting popn
+    unobspop <- sim.popn(0, attr(obspop, 'boundingbox'), buffer = 0, covariates = NULL) 
+    CH <- object$capthist
+    
+    if (dN) {
+        # conceptually, (sampling) variation in D affects only the N-n unobserved
+        detpar <- detectpar(object, byclass = TRUE, bytrap = bytrap) 
+        ## if bytrap, detpar[[i]] is trap x parameter dataframe for class i
+        if (is.na(detpar[[1]]['pmix'])) detpar <- list(c(detpar, pmix=1))
+        for (i in 1:length(detpar)) {
+            pd <- pdot(X = mask, traps = traps(CH), detectfn = object$detectfn,
+                       detectpar = detpar[[i]], noccasions = ncol(CH), ncores = NULL)
+            # sample locations of unobserved AC for i-th class
+            pop <- sim.popn (D = D * (1 - pd) * detpar[[i]]$pmix, core = mask, model2D = 'IHP', 
+                             Ndist = 'poisson', covariates = NULL)
+            if (nrow(pop)>0) unobspop <- rbind(unobspop, pop)
+        }
+        if (nrow(unobspop)>0) {
+            rownames(unobspop) <- paste0('N', 1:nrow(unobspop))
+        }
+    }
+    else {
         N <- sum(D) * attr(mask, 'area')
-
-        # make a null starting popn
-        unobspop <- sim.popn(0, data.frame(x=0,y=0), covariates = NULL) 
-
         if (N>nrow(CH)) {
-            detpar <- detectpar(object, byclass = TRUE)  
+            detpar <- detectpar(object, byclass = TRUE, bytrap = bytrap) 
+            ## if bytrap, detpar[[i]] is trap x parameter dataframe for class i
             if (is.na(detpar[[1]]['pmix'])) detpar <- list(c(detpar, pmix=1))
             for (i in 1:length(detpar)) {
                 pd <- pdot(X = mask, traps = traps(CH), detectfn = object$detectfn,
-                           detectpar = detpar[[i]], noccasions = ncol(CH), ncores = ncores)
+                           detectpar = detpar[[i]], noccasions = ncol(CH), ncores = NULL)
                 # sample locations of unobserved AC for i-th class
                 pop <- sim.popn (D = D * (1 - pd) * detpar[[i]]$pmix, core = mask, model2D = 'IHP', 
                                  Ndist = 'fixed', covariates = NULL)
-                unobspop <- rbind(unobspop, pop)
+                if (nrow(pop)>0) unobspop <- rbind(unobspop, pop)
             }
             rownames(unobspop) <- paste0('N', 1:nrow(unobspop))
         }
@@ -71,157 +86,214 @@ MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
             if (N<nrow(CH)) warning("proposed N less than number observed; unobserved not simulated")
             else warning("proposed N equal to number observed; unobserved not simulated")
         }
-        
-        # -----------------------------------
-        # combine observed, unobserved
-        popn <- rbind(obspop, unobspop, renumber = FALSE)
-        Nobs <- nrow(obspop)
-        Nunobs <- nrow(unobspop)  # varies because D varies
-        covariates(popn) <- data.frame(obs = rep(c(TRUE,FALSE), c(Nobs, Nunobs)))
-        
-        # -----------------------------------
-        ## add individual covariates that may be needed by sim.detect
-        if (!is.null(object$hcov)) {
-            ## for unobs, sample with replacement from original hcov field
-            oldhcov <- covariates(CH)[,object$hcov]
-            covariates(popn)[[object$hcov]] <- 
-                sample(oldhcov, size = nrow(popn), replace = TRUE)
-            ## then overwrite obs (known) hcov
-            covariates(popn)[1:Nobs,object$hcov] <- covariates(object$capthist)[,object$hcov]
-        }
-        
-        # -----------------------------------
-        # using sim.detect revised 2024-08-02
-        simCH <- sim.detect(object, popnlist = list(popn), expected = TRUE, 
-                            dropzeroCH = FALSE, renumber = FALSE)
-        expCH <- attr(simCH, 'expected')
+    }
+    
+    # -----------------------------------
+    # combine observed, unobserved
+    popn <- rbind(obspop, unobspop, renumber = FALSE)
+    Nobs <- nrow(obspop)
+    Nunobs <- nrow(unobspop)  # varies because D varies
+    covariates(popn) <- data.frame(obs = rep(c(TRUE,FALSE), c(Nobs, Nunobs)))
+    
+    # -----------------------------------
+    ## add individual covariates that may be needed by sim.detect
+    if (!is.null(object$hcov)) {
+        ## for unobs, sample with replacement from original hcov field
+        oldhcov <- covariates(CH)[,object$hcov]
+        covariates(popn)[[object$hcov]] <- 
+            sample(oldhcov, size = nrow(popn), replace = TRUE)
+        ## then overwrite obs (known) hcov
+        covariates(popn)[1:Nobs,object$hcov] <- covariates(object$capthist)[,object$hcov]
+    }
+    
+    popn
+}
 
-        Tsim <- statfn(simCH)
-        Texp <- statfn(expCH)
+MCgof <- function (object, nsim = 100, statfn = NULL, testfn = NULL,
+                   seed = NULL, ncores = 1, clustertype = c("PSOCK","FORK"), 
+                   usefxi = TRUE, useMVN = TRUE, quiet = FALSE)
+    
+{
+    if (inherits(object, 'secrlist')) {
+        out <- lapply(object, MCgof, 
+                      nsim = nsim, statfn = statfn, testfn = testfn,
+                      seed = seed, ncores = ncores, clustertype = clustertype, 
+                      usefxi = usefxi, useMVN = useMVN, quiet = quiet)
+        names(out) <- names(object)
+        out
+    }
+    else {
         
-        if (!quiet && ncores == 1) setTxtProgressBar(pb, i)
+        #---------------------------------------------------------------------------
+        ## function to run one replicate
+        runone <- function(i=1) {
+            # -----------------------------------------------
+            # randomize parameter values from MVN
+            if (useMVN) {
+                object$fit$par <- MASS::mvrnorm(
+                    n     = 1,
+                    mu    = object$fit$par, 
+                    Sigma = object$beta.vcv)
+            }
+            # -----------------------------------------------
+            # draw randomized AC
+            if (usefxi) {
+                popn <- simfxiAC(object, bytrap)
+            }
+            else {
+                Darray <- getDensityArray (predictDsurface(object))
+                popn <- sim.onepopn(object, Darray)[[1]]
+            }
             
-        list(Tsim = Tsim, Texp = Texp, par = object$fit$par, popn = popn)
+            # -----------------------------------------------
+            # simulate detections for these parameters and AC
+            simCH <- sim.detect(
+                object     = object, 
+                popnlist   = list(popn), 
+                expected   = TRUE, 
+                dropzeroCH = FALSE, 
+                renumber   = FALSE)
+            
+            # -----------------------------------------------
+            # expected counts for these parameters and AC
+            
+            # if usefxi = FALSE the 'expected' count is for a random AC
+            # and the comparison may not be meaningful
+            
+            expCH <- attr(simCH, 'expected')
+            
+            Tsim <- statfn(simCH)
+            Texp <- statfn(expCH)
+            
+            if (!quiet && ncores == 1) setTxtProgressBar(progressbar, i)
+            list(Tsim = Tsim, Texp = Texp, par = object$fit$par, popn = popn)
+            
+        }   # end of runone
         
-    }   # end of runone
-
-    #---------------------------------------------------------------------------
-    # construct test statistic for simulation 'capti' and marginal counts 'stat'
-    ft <- function (capti, stat) {
-        N <- nrow(capti$popn)  # varies for each replicate because D varies
-        n <- nrow(CH)
-        # addzeroCH is internal secr function
-        paddedCH <- addzeroCH(CH, N - n, prefix = 'N')
-        Tobs <- statfn(paddedCH)
-        obs <- testfn(Tobs[[stat]],       capti$Texp[[stat]])
-        sim <- testfn(capti$Tsim[[stat]], capti$Texp[[stat]])
-        c(Tobs = obs, Tsim = sim, p = sim>obs)  
-    }   # end of ft
-    #---------------------------------------------------------------------------
-    
-    ## create default statfn if needed
-    if (is.null(statfn)) {
-        statfn <- defaultstatfn
-    }
-    ## create default testfn if needed
-    if (is.null(testfn)) {
-        testfn <- defaulttestfn
-    }
-    
-    #---------------------------------------------------------------------------
-    ##  check input
-    if (any(c("bn", "bkn", "bkc", "Bkc") %in% tolower(object$vars)))
-        stop ("MCgof works only with binary behavioural responses")
-    
-    if (!inherits(object,'secr'))
-        stop ("requires 'secr' object")
-    
-    if (object$CL)
-        stop ("MCgof not implemented for conditional likelihood")
-    
-    if (ms(object$capthist))
-        stop ("MCgof not implemented for multi-session data")
-    
-    if (!is.null(object$groups)) {
-        stop ("MCgof not implemented for groups")
-    }
-    
-    #---------------------------------------------------------------------------
-
-    if (any(detector(traps(object$capthist)) == 'single')) {
-        # expected values not available for 'single'
-        warning("replacing single detector type with multi - results unreliable")
-        detector(traps(object$capthist)) <- 'multi'
-    }
-    
-    #---------------------------------------------------------------------------
-    ## set random seed
-    ## copied from simulate.lm
-    if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
-        runif(1)
-    if (is.null(seed))
-        RNGstate <- get(".Random.seed", envir = .GlobalEnv)
-    else {
-        R.seed <- get(".Random.seed", envir = .GlobalEnv)
-        set.seed(seed)
-        RNGstate <- structure(seed, kind = as.list(RNGkind()))
-        on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
-    }
-    #---------------------------------------------------------------------------
-    
-    ## action starts here
-    
-    ptm  <- proc.time()
-    
-    CH <- object$capthist
-    
-    # ----------------------------------------------------------
-    # perform nsim simulations
-    ## parallel processing optional - often slower
-
-    if (is.null(ncores)) ncores <- setNumThreads()  # default is 1
-    if (!quiet && ncores == 1) pb <- txtProgressBar(0, nsim, style = 3)
-
-    if (ncores > 1) {
-        clustertype <- if (.Platform$OS.type == "unix") "FORK" else "PSOCK"
-        clust <- makeCluster(ncores, type = clustertype)
-        if (clustertype == "PSOCK") {
-            clusterExport(clust, c("object", "CH", "statfn"), environment())
+        #---------------------------------------------------------------------------
+        # construct test statistic for simulation 'capti' and marginal counts 'stat'
+        ft <- function (capti, stat) {
+            N <- nrow(capti$popn)  # varies for each replicate because D varies
+            CH <- object$capthist
+            n <- nrow(CH)
+            # addzeroCH is internal secr function
+            paddedCH <- addzeroCH(CH, N - n, prefix = 'N')
+            Tobs <- statfn(paddedCH)
+            obs <- testfn(Tobs[[stat]],       capti$Texp[[stat]])
+            sim <- testfn(capti$Tsim[[stat]], capti$Texp[[stat]])
+            c(Tobs = obs, Tsim = sim, p = sim>obs)  
+        }   # end of ft
+        #---------------------------------------------------------------------------
+        
+        ## create default statfn if needed
+        if (is.null(statfn)) {
+            statfn <- defaultstatfn
         }
-        clusterSetRNGStream(clust, seed)
-        on.exit(stopCluster(clust))
-        capt <- parLapply(clust, 1:nsim, runone)
+        ## create default testfn if needed
+        if (is.null(testfn)) {
+            testfn <- defaulttestfn
+        }
         
-    }
-    else {
-        capt <- lapply(1:nsim, runone)
-    }
-    
-    # ----------------------------------------------------------
-    # apply function 'ft' for each count vector 'stat'
-    # stats correspond to named components of statfn output list
-    stats <- c('yik','yi','yk')
-    all <- lapply(stats, function(x) sapply (capt, ft, stat = x))
-    names(all) <- stats
-    
-    # ----------------------------------------------------------
-    # wrap up
-    out <- list(
-        statfn   = statfn,
-        testfn   = testfn,
-        all      = all,
-        nsim     = nsim,
-        means    = sapply(all, apply, 1, mean), 
-        proctime = (proc.time() - ptm)[3])
-    class(out) <- 'MCgof'
-    
-    if (!quiet) {
-        if (ncores==1) close(pb)
-        message ("MCgof completed in ", round((proc.time() - ptm)[3],3), " seconds")
-        print(summary(out))
-    }
-    
-    invisible(out)
-    
+        #---------------------------------------------------------------------------
+        ##  check input
+        if (any(c("bn", "bkn", "bkc", "Bkc") %in% tolower(object$vars)))
+            stop ("MCgof works only with binary behavioural responses")
+        
+        if (!inherits(object,'secr'))
+            stop ("requires 'secr' object")
+        
+        if (object$CL)
+            stop ("MCgof not implemented for conditional likelihood")
+        
+        if (ms(object$capthist))
+            stop ("MCgof not implemented for multi-session data")
+        
+        if (!is.null(object$groups)) {
+            stop ("MCgof not implemented for groups")
+        }
+        
+        #---------------------------------------------------------------------------
+        
+        if (any(detector(traps(object$capthist)) == 'single')) {
+            # expected values not available for 'single'
+            warning("replacing single detector type with multi - results unreliable")
+            detector(traps(object$capthist)) <- 'multi'
+        }
+        #---------------------------------------------------------------------------
+        
+        clustertype <- match.arg(clustertype)
+        
+        #---------------------------------------------------------------------------
+        ## set random seed
+        ## copied from simulate.lm
+        if (!exists(".Random.seed", envir = .GlobalEnv, inherits = FALSE))
+            runif(1)
+        if (is.null(seed))
+            RNGstate <- get(".Random.seed", envir = .GlobalEnv)
+        else {
+            R.seed <- get(".Random.seed", envir = .GlobalEnv)
+            set.seed(seed)
+            RNGstate <- structure(seed, kind = as.list(RNGkind()))
+            on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
+        }
+        #---------------------------------------------------------------------------
+        
+        ## action starts here
+        
+        ptm  <- proc.time()
+        
+        ## Does model have differences in detection parameters between traps?
+        ## this is conditional to save time in pdot
+        bytrap <- length(unique(apply(object$design0$PIA, 4, mean)))>1
+        
+        # ----------------------------------------------------------
+        # perform nsim simulations
+        ## parallel processing optional - often slower
+        
+        if (is.null(ncores)) ncores <- setNumThreads()  # default is 1
+        if (!quiet && ncores == 1) progressbar <- txtProgressBar(0, nsim, style = 3)
+        
+        if (ncores > 1) {
+            if (.Platform$OS.type != "unix") clustertype <- "PSOCK"
+            clust <- makeCluster(ncores, type = clustertype)
+            if (clustertype == "PSOCK") {
+                clusterExport(clust, c("object", "simfxiAC", "statfn", "bytrap"), 
+                              environment())
+            }
+            clusterSetRNGStream(clust, seed)
+            on.exit(stopCluster(clust))
+            capt <- parLapply(clust, 1:nsim, runone)
+            
+        }
+        else {
+            capt <- lapply(1:nsim, runone)
+        }
+        
+        # ----------------------------------------------------------
+        # apply function 'ft' for each count vector 'stat'
+        # stats correspond to named components of statfn output list
+        stats <- names(statfn(object$capthist))
+        all <- lapply(stats, function(x) sapply (capt, ft, stat = x))
+        names(all) <- stats
+        
+        # ----------------------------------------------------------
+        # wrap up
+        out <- list(
+            nsim     = nsim,
+            statfn   = statfn,
+            testfn   = testfn,
+            all      = all,
+            proctime = (proc.time() - ptm)[3])
+        class(out) <- 'MCgof'
+        
+        if (!quiet) {
+            if (ncores==1) close(progressbar)
+            message ("MCgof for ", nsim, " simulations completed in ", 
+                     round((proc.time() - ptm)[3],3), " seconds")
+            print(summary(out))
+        }
+        
+        invisible(out)
+    }    
 }
 ################################################################################
--- R/MCgof.methods.R
+++ R/MCgof.methods.R
@@ -1,16 +1,19 @@
 # MCgof methods
 
-plot.MCgof <- function(x, counts = 'all', overlay = NULL, ...) {
+plot.MCgof <- function(x, counts = 'all', overlay = NULL, maxT = NULL, main = NULL, cex = 0.9, ...) {
     onestat <- function (count) {
         xy <- x$all[[count]]    # dimensions c(Tsim, Tobs, p), 1:nreplicates)
-        lim <- range(xy[1:2,])
+        if (!is.null(maxT)) xy[xy>maxT] <- NA
+        lim <- range(xy[1:2,], na.rm = TRUE)
         lim <- lim + c(-1, +1) * diff(lim)/7
         MASS::eqscplot(xy['Tsim',], xy['Tobs',], 
                        xlab = 'simulated', ylab = 'observed',
                        xlim = lim, ylim = lim)
         
         abline(0,1, col = 'red')
-        mtext(side=3, paste(main[count], " p =", round(mean(xy[3,]),3)))
+        if (any(main != "")) {
+            mtext(side=3, line=0.4, paste(main[count], " p =", round(summ['p',count],3)), cex = cex)
+        }
         
         # optional overlay of points from another MCgof or from scrgof
         if (!is.null(overlay)) {
@@ -24,22 +27,37 @@ plot.MCgof <- function(x, counts = 'all', overlay = NULL, ...) {
             }
         }        
     }
-    main <- c(yik = 'individual-detector', yi = 'individual', yk = 'detector')
-    if (tolower(counts) == 'all') counts <- names(x$all)
+    if (is.null(main)) {
+        # default vector of potential labels
+        main <- c(yik = 'yik individual x detector', 
+                  yi = 'yi individual', 
+                  yk = 'yk detector')
+    }
+    opar <- par(cex = cex)
+    on.exit(par(opar))
+    if (tolower(counts[1]) == 'all') counts <- names(x$all)
+    summ <- summary(x)
     junk <- lapply(counts, onestat)
     invisible()
 }
 ################################################################################
 
 summary.MCgof <- function(object, ...) {
-    temp <- object[c('nsim', 'means','proctime')]
-    class(temp) <- 'summary.MCgof'
-    temp
+    summ <- sapply(object$all, apply, 1, median, na.rm = TRUE)
+    summ['p',] <- sapply(object$all, function(x) mean(x['p',], na.rm = TRUE))
+    summ <- rbind(summ, validn = sapply(object$all, function(x) sum(!is.na(x['p',]))))
+    class(summ) <- 'summary.MCgof'
+    summ
 }
 ################################################################################
 
 print.summary.MCgof <- function (x, ...) {
-    cat (paste0('nsim = ', x$nsim, ', proctime = ', round(x$proctime, 2), ' seconds\n'))
-    print(x$means)
+    class(x) <- NULL
+    print(x)
+}
+################################################################################
+
+print.MCgof <- function (x, ...) {
+    print(summary(x))
 }
 ################################################################################
