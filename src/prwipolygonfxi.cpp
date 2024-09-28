f2dde3303f3841a4ab29cdcce0758d10e8c96ef7
MurrayEfford <murray.efford@otago.ac.nz>
2024-09-29 10:15:41 +1300

    reorder includes

--- src/prwipolygonfxi.cpp
+++ src/prwipolygonfxi.cpp
@@ -1,6 +1,6 @@
-#include <Rcpp.h>
-#include <RcppParallel.h>
+// #include <Rcpp.h>
 #include "secr.h"
+#include <RcppParallel.h>
 
 //==============================================================================
 // 2020-01-26
