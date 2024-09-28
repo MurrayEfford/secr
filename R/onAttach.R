7bedccbf1a1be6991d7089a695924e16a4ba486f
MurrayEfford <murray.efford@otago.ac.nz>
2024-09-29 08:47:06 +1300

    remove \keyword from Rd

--- R/onAttach.R
+++ R/onAttach.R
@@ -1,13 +1,14 @@
 ###############################################################################
 ## package 'secr'
 ## onAttach.R
-## last changed 2011-06-16 2013-04-20 2016-10-07
+## last changed 2011-06-16 2013-04-20 2016-10-07 2024-09-28
 ###############################################################################
 
 .onAttach <- function (libname, pkgname) {
     version <- paste0(packageVersion('secr'), .localstuff$packageType)
     packageStartupMessage( "This is secr ", version,
-                           ". For overview type ?secr" )
+                           ". For overview type ?secr\n", 
+                           "esa.plot and some fxi functions have new names; see ?version5" )
 }
 
 ## .onLoad is preferred if actions are required for single functions 
