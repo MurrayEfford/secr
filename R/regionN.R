2462d232af4c8282a8dcbac43f27bdf21261d931
MurrayEfford <murray.efford@otago.ac.nz>
2025-01-10 10:42:06 +1300

    discard sessnum arg

--- R/regionN.R
+++ R/regionN.R
@@ -217,7 +217,7 @@ region.N.secr <- function (object, region = NULL, spacing = NULL, session = NULL
                 if (object$CL) {
                     warning("using approximate conversion of conditional to full", 
                             " model for variances")
-                    object <- completeDbeta(object, sessnum, vcv = TRUE)
+                    object <- completeDbeta(object, vcv = TRUE)
                 }
                 dENdphi <- nlme::fdHess (object$fit$par[indx],
                                          betaEN, object = object, region = regionmask, group = group,
