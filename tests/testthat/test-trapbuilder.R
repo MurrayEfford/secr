74ffb8e100f793eaea4e89d151cd5c58758a9726
MurrayEfford <murray.efford@otago.ac.nz>
2025-03-27 07:50:53 +1300

    test of suggested package spsurvey now conditional

--- tests/testthat/test-trapbuilder.R
+++ tests/testthat/test-trapbuilder.R
@@ -17,10 +17,14 @@ test_that("method = 'SRS' works", {
     expect_equal(spacing(sample0), 76.3612953003, tolerance = 1e-4, check.attributes = FALSE)
 })
 
-
-test_that("method = 'GRTS' works", {
-    set.seed(1235)
-    # GRTS sample
-    expect_silent(sample1 <- trap.builder(n = 32, region = OVforest, method = 'GRTS'))
-    expect_equal(spacing(sample1), 94.7225747307, tolerance = 1e-4, check.attributes = FALSE)
-})
+# conditional from 2025-03-27 (see )
+if (requireNamespace("spsurvey", versionCheck=list(op=NULL, version = ">=5.3.0"), quietly = TRUE)) {
+    
+    test_that("method = 'GRTS' works", {
+        set.seed(1235)
+        # GRTS sample
+        expect_silent(sample1 <- trap.builder(n = 32, region = OVforest, method = 'GRTS'))
+        expect_equal(spacing(sample1), 94.7225747307, tolerance = 1e-4, check.attributes = FALSE)
+    })
+    
+}
