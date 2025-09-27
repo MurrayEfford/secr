36888bdef5e74ab5d3714c1baf9ff8cab4671a64
MurrayEfford <murray.efford@otago.ac.nz>
2025-09-27 18:41:57 +1200

    shelve slow polygon LL test

--- tests/testthat/test-initial.R
+++ tests/testthat/test-initial.R
@@ -55,25 +55,27 @@ test_that("correct rejection of duplicates at exclusive detectors", {
 ###############################################################################
 ## Multi-polygon bug 2021-05-18
 
-datadir <- system.file("extdata", package = "secr")
-polyexample1 <- read.traps(file = paste0(datadir, '/polygonexample1.txt'), 
-    detector = 'polygon')
-polygonCH <- sim.capthist(polyexample1, popn = list(D = 1, buffer = 200),
-    detectfn = 'HHN', detectpar = list(lambda0 = 5, sigma = 50),
-    noccasions = 1, seed = 123)
+# test shelved 2025-09-27 as takes about 60 seconds
 
-test_that("simulated polygon data have correct RPSV", {
-    rpsv <- RPSV(polygonCH, CC = TRUE)
-    expect_equal(rpsv, 45.16401, tolerance = 1e-4, check.attributes = FALSE)
-})
-
-test_that("correct likelihood (multi-detector polygon data)", {
-    args <- list(capthist = polygonCH, buffer = 200, detectfn = 'HHN',
-        start = list(D=1, lambda0=5, sigma = 50), verify = FALSE, 
-        details = list(LLonly = TRUE))
-    LL <- do.call(secr.fit, args)[1]
-    expect_equal(LL, -2026.92169, tolerance = 1e-4, check.attributes = FALSE)
-})
+# datadir <- system.file("extdata", package = "secr")
+# polyexample1 <- read.traps(file = paste0(datadir, '/polygonexample1.txt'), 
+#     detector = 'polygon')
+# polygonCH <- sim.capthist(polyexample1, popn = list(D = 1, buffer = 200),
+#     detectfn = 'HHN', detectpar = list(lambda0 = 5, sigma = 50),
+#     noccasions = 1, seed = 123)
+# 
+# test_that("simulated polygon data have correct RPSV", {
+#     rpsv <- RPSV(polygonCH, CC = TRUE)
+#     expect_equal(rpsv, 45.16401, tolerance = 1e-4, check.attributes = FALSE)
+# })
+# 
+# test_that("correct likelihood (multi-detector polygon data)", {
+#     args <- list(capthist = polygonCH, buffer = 200, detectfn = 'HHN',
+#         start = list(D=1, lambda0=5, sigma = 50), verify = FALSE, 
+#         details = list(LLonly = TRUE))
+#     LL <- do.call(secr.fit, args)[1]
+#     expect_equal(LL, -2026.92169, tolerance = 1e-4, check.attributes = FALSE)
+# })
 
 ###############################################################################
 ## join nonspatial bug 2021-05-28
