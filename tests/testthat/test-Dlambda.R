38ae72f9099fa1fc0731a9a253d07719b535d31a
MurrayEfford <murray.efford@otago.ac.nz>
2026-01-20 15:10:59 +1300

    extended Dlambda tests

--- tests/testthat/test-Dlambda.R
+++ tests/testthat/test-Dlambda.R
@@ -10,11 +10,25 @@ Sys.setenv(RCPP_PARALLEL_BACKEND = "tinythread")
 msk <- make.mask(traps(ovenCH[[1]]), buffer = 300, type='trapbuffer', nx = 32)
 
 test_that("Dlambda likelihood correct", {
-    LL1 <- secr.fit(ovenCH, model = D~1, mask = msk, trace = TRUE, 
+    LL.0 <- secr.fit(ovenCH, model = D~1, mask = msk, trace = TRUE, 
                     details = list(Dlambda = TRUE, LLonly = TRUE), 
                     start = c(0,0,-3.5,4), ncores = 2)
-    expect_equal(LL1, -956.66706, tolerance = 1e-4, check.attributes = FALSE)
+    LL.CL <- secr.fit(ovenCH, model = D~1, mask = msk, trace = TRUE, 
+                    details = list(Dlambda = TRUE, LLonly = TRUE), 
+                    start = c(0,0,-3.5,4), ncores = 2, CL = TRUE)
+    expect_equal(LL.0,  -956.66706, tolerance = 1e-4, check.attributes = FALSE)
+    expect_equal(LL.CL, -946.41862, tolerance = 1e-4, check.attributes = FALSE)
 })
 
-# possibly include Dlambda fitted model in ovenbird, and
-#predictDlambda(fit1)
+# now with fitted models included in package
+# ovenbird.model.D, ovenbird.model.Dl
+test_that("Ovenbird Dlambda correct", {
+    expect_equal(
+        predictDlambda(ovenbird.model.Dl)[,1],  
+        c(1.0322172, 0.9381376, 0.9381376, 0.9381376, 0.9381376), 
+        tolerance = 1e-5, check.attributes = FALSE)
+    expect_equal(
+        predictDlambda(ovenbird.model.Dl)[2,1],  
+        exp(coef(ovenbird.model.D)[2,1]), 
+        tolerance = 1e-5, check.attributes = FALSE)
+})
