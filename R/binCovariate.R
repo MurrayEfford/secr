ee599892d0f2da9928af568ef74eacc105e4b1eb
MurrayEfford <murray.efford@otago.ac.nz>
2026-03-26 12:44:25 +1300

    extend binCovariate() with new arguments 'breaks' and 'replace'

--- R/binCovariate.R
+++ R/binCovariate.R
@@ -8,23 +8,33 @@ bin <- function(x, width) {
     width * (trunc(x / width)) + width/2
 }
 
-binCovariate <- function (object, covname, width) {
+binCovariate <- function (object, covname, width, breaks = NULL, replace = FALSE, ...) {
     if (ms(object)) {
         # replace components while retaining attributes (e.g., names)
-        object[] <- lapply(object, binCovariate, covname, width)
+        object[] <- lapply(object, binCovariate, covname, width, breaks, replace, ...)
         object
     }
     else {
-        newname <- paste0(covname, width)
+        if (is.null(breaks)) {
+            newname <- paste0(covname, width)
+        }
+        else {
+            newname <- paste0(covname, 'cut')
+        }
         if (is.null(covariates(object)))
             stop ("object has no covariates")
         if ( !covname %in% names(covariates(object)))
             stop ("covariate ", covname, " not found")
         if (!is.numeric(covariates(object)[,covname]))
             stop ("covariate is not numeric")
-        if (newname %in% names(covariates(object)))
+        if (!replace && (newname %in% names(covariates(object))))
             stop ("covariate ", newname, " already exists")
-        covariates(object)[,newname] <- bin(covariates(object)[,covname], width)
+        if (is.null(breaks)) {    
+            covariates(object)[,newname] <- bin(covariates(object)[,covname], width)
+        }
+        else {
+            covariates(object)[,newname] <- cut(covariates(object)[,covname], breaks, ...)
+        }
         object
     }
 }
