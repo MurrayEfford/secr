e06a780c9fcd11f8cd013d23c8ddf789082e1411
MurrayEfford <murray.efford@otago.ac.nz>
2026-07-18 22:48:45 +1200

    allow secrlist input

--- R/RSE.R
+++ R/RSE.R
@@ -4,45 +4,50 @@
 ## 2020-05-06 RSE may return matrix of values
 
 RSE <- function (fit, parm = NULL, newdata = NULL) {
-    if (is.null(parm)) {
-        parm <- names(fit$parindx)
-    }
-    if (length(parm) > 1) {
-        sapply(parm, RSE, fit = fit, newdata = newdata)
+    if (inherits(fit, 'secrlist')) {
+        lapply (fit, RSE, parm = parm, newdata = newdata)
     }
     else {
-        ## condition ensures 
-        ## (i) parm in model (ii) no modelled variation in parm
-        pindex <- length(fit$parindx[[parm]])
-        if (pindex == 0) {
-            warning ("parameter ", parm, " not in model")
+        if (is.null(parm)) {
+            parm <- names(fit$parindx)
         }
-        else if (pindex == 1 && fit$link[[parm]] == 'log') {
-            if (!is.null(newdata)) {
-                warning ("non-null newdata ignored for ", parm)
-            }
-            ## Efford & Boulanger MEE 2019
-            sqrt(exp(vcov(fit)[parm, parm])-1)    
+        if (length(parm) > 1) {
+            sapply(parm, RSE, fit = fit, newdata = newdata)
         }
         else {
-            if (pindex > 1 && is.null(newdata)) {
-                warning ("parameter ", parm, " varies in model; consider specifying newdata")
+            ## condition ensures 
+            ## (i) parm in model (ii) no modelled variation in parm
+            pindex <- length(fit$parindx[[parm]])
+            if (pindex == 0) {
+                warning ("parameter ", parm, " not in model")
             }
-            pred <- predict(fit, newdata = newdata)
-            if (parm %in% rownames(pred)) {
-                pred[parm, 'SE.estimate'] / pred[parm, 'estimate']
-            }
-            else if (parm %in% rownames(pred[[1]])) {
-                # modified 2020-05-06 to return all
-                getone <- function (x) {
-                    x[parm, 'SE.estimate'] / x[parm, 'estimate']
+            else if (pindex == 1 && fit$link[[parm]] == 'log') {
+                if (!is.null(newdata)) {
+                    warning ("non-null newdata ignored for ", parm)
                 }
-                sapply(pred, getone)
+                ## Efford & Boulanger MEE 2019
+                sqrt(exp(vcov(fit)[parm, parm])-1)    
             }
-            else if (parm %in% names(pred)) {
-                pred[[parm]][1,'SE.estimate'] / pred[[parm]][1,'estimate']
+            else {
+                if (pindex > 1 && is.null(newdata)) {
+                    warning ("parameter ", parm, " varies in model; consider specifying newdata")
+                }
+                pred <- predict(fit, newdata = newdata)
+                if (parm %in% rownames(pred)) {
+                    pred[parm, 'SE.estimate'] / pred[parm, 'estimate']
+                }
+                else if (parm %in% rownames(pred[[1]])) {
+                    # modified 2020-05-06 to return all
+                    getone <- function (x) {
+                        x[parm, 'SE.estimate'] / x[parm, 'estimate']
+                    }
+                    sapply(pred, getone)
+                }
+                else if (parm %in% names(pred)) {
+                    pred[[parm]][1,'SE.estimate'] / pred[[parm]][1,'estimate']
+                }
+                else NA
             }
-            else NA
         }
     }
 }
